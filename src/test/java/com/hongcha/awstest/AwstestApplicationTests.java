e4338d4b30333dc5571cc8dc5e1dd6a39e7b5f1b
MinkyungChoi <minkky330@gmail.com>
2020-09-21 14:52:23 +0900

    200921 AWS 배포 테스트하기

---
+++ src/test/java/com/hongcha/awstest/AwstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongcha.awstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwstestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
